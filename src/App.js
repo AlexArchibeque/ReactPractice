fc99da9cdef20e992aa3bc8de99e7365fdc73a22
AlexArchibeque <wonkman33@gmail.com>
2021-01-21 01:01:21 -0800

    react state tutorial part 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import React, {useState} from 'react';
 
-function App() {
+// const expensiveInitialState = () => {
+//   return 10;
+// }
+
+const App = () => {
+
+  // const [{ count, count2 }, setCount] = useState({ count: 10, count2: 20})
+  const [count, setCount] = useState(10);
+  const [count2, setCount2] = useState(20);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button 
+        onClick={() =>{
+          setCount(c => c + 1)
+          setCount2(c => c + 1)
+        }}>
+          Increase Count
+          </button>
+      <div>count 1: {count}</div>
+      <div>count 2: {count2}</div>
+      {/* <div> hello </div> */}
     </div>
   );
 }
